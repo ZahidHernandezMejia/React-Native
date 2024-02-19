6a08c058a17f176d938063825b3721eb56e3bade
Zahid <zahidhm.dp@gmail.com>
2024-02-19 15:51:03 -0600

    Paginacion

--- my-app/src/Components/Usuarios.tsx
+++ my-app/src/Components/Usuarios.tsx
@@ -1,36 +1,69 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { reqResApi } from "../api/reqRes";
-import { ReqRespListado, Usuario } from '../interfaces/reqRes';
+import { ReqRespListado, Usuario } from "../interfaces/reqRes";
 
 export const Usuarios = () => {
   const [usuarios, setUsuarios] = useState<Usuario[]>([]);
 
+  // Referencia a una variable que si cambia su valor sigue
+  // siendo la misma pero no dispara el procedimiento para renderizar el componente
+  const paginaRef = useRef(1);
+
   // async y await no debe de ir en un useEffect
   useEffect(() => {
-    reqResApi
-      .get<ReqRespListado>("/users")
-      .then((resp) => {
-        console.log(resp.data.data);
-        setUsuarios(resp.data.data);  
-      })
-      .catch(console.log);
+    // llamado de api
+    return()=>{
+      cargarUsuarios();
+    }
   }, []);
+  // Ponemos el return dentro del useEffect o bien hacemos otra funcion que ponemos en el onClick
+  // const handleSiguienteClick = () => {
+    // paginaRef.current++;
+    // cargarUsuarios();
+  // };
+  const cargarUsuarios = async () => {
+    const resp = await reqResApi.get<ReqRespListado>("/users", {
+      params: {
+        // :Objeto.Valor
+        page: paginaRef.current,
+      },
+    });
 
-  const renderItem=({id,first_name,last_name,email,avatar}:Usuario)=>{
+    if (resp.data.data.length > 0) {
+      setUsuarios(resp.data.data);
+      paginaRef.current++;
+      console.log(paginaRef.current);
+    } else {
+      alert("No hay mas usuarios");
+    }
+    
+  };
+  const renderItem = ({
+    id,
+    first_name,
+    last_name,
+    email,
+    avatar,
+  }: Usuario) => {
     return (
       <tr key={id.toString()}>
         <td>
-          <img style={{
-            width:45,
-            borderRadius:100,
+          <img
+            style={{
+              width: 45,
+              borderRadius: 100,
             }}
-          src={avatar} alt={first_name} />
+            src={avatar}
+            alt={first_name}
+          />
         </td>
-        <td>{first_name} {last_name}</td>
+        <td>
+          {first_name} {last_name}
+        </td>
         <td>{email}</td>
       </tr>
-    )
-  }
+    );
+  };
   return (
     <>
       <h3>Usuarios</h3>
@@ -44,13 +77,12 @@ export const Usuarios = () => {
         </thead>
         <tbody>
           {
-            usuarios.map( renderItem )
+            usuarios.map(renderItem)
             // usuarios.map((usuario:Usuario)=>renderItem(usuario))
           }
         </tbody>
       </table>
-      <button
-      className="btn btn-primary">
+      <button className="btn btn-primary" onClick={cargarUsuarios}>
         Siguientes
       </button>
     </>
